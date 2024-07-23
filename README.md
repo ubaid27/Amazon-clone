417a69bfad943dca12e2106b19865460da4c4a6c
Ubaid Ur Rehman <ubaid277rehman@gmail.com>
2024-07-23 16:27:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Amazon-clone
+ An Amazon clone front-end webpage designed using HTML, CSS, and JavaScript. It features a responsive design, product sliders, and various interactive elements, mimicking the look and feel of Amazon's homepage. This project is static and focuses on visual and layout aspects without backend integration.
---
+++ script.js
@@ -0,0 +1,39 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelector('.control-prev');
+const next_btn = document.querySelector('.control-next');
+
+let n = 0;
+
+function changeSlider(){
+    for (let i=0; i<imgs.length; i++){
+        imgs[i].style.display = 'none';
+    }
+
+    imgs[n].style.display = 'block';
+}
+changeSlider();
+
+prev_btn.addEventListener('click', (e)=>{
+    if(n > 0 ){
+        n--;
+    }else{
+        n = imgs.length - 1;
+    }
+    changeSlider();
+})
+next_btn.addEventListener('click', (e)=>{
+    if(n < imgs.length - 1 ){
+        n++;
+    }else{
+        n = 0;
+    }
+    changeSlider();
+})
+
+const scrollContainer = document.querySelectorAll('.products');
+for (const item of scrollContainer) {
+    item.addEventListener('wheel', (e) => {
+        e.preventDefault();
+        item.scrollLeft += e.deltaY;
+    });
+}
